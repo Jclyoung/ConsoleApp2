699bd1d6b333a771275422e6276c16814b746675
Jennifer Young <jclyoung@gmail.com>
2019-03-03 09:56:20 -0600

    3/3/19

--- BottlesOfBeerOnTheWall/Program.cs
+++ BottlesOfBeerOnTheWall/Program.cs
@@ -15,12 +15,10 @@ namespace BottlesOfBeerOnTheWall
                 Console.WriteLine( i + " bottles of beer on the wall, " + i + " bottles of beer.");
                 i--; Console.WriteLine(" Take one done and pass it around " + i + " bottles of beer on the wall.");
 
-                i++; if (i == 3)
+                i++; if (i == 1)
 
                 {   
-                    Console.WriteLine("2 bottles of beer on the wall, 2 bottles of beer.");
-                    Console.WriteLine("Take one down pass it around 1 bottle of beer on the wall.");
-                    Console.WriteLine("Take one down and pass it around and now there are no more bottles of beer on the wall!");
+                    
 
                     Console.ReadLine();
                     break;
